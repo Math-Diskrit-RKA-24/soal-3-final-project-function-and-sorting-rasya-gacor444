91d010439a3aa60929dd0456d200929b86a70765
Paibull <90700967+Paibull@users.noreply.github.com>
2024-12-04 08:52:46 +0700

    Update game.py

--- src/game.py
+++ src/game.py
@@ -35,18 +35,16 @@ def attackPlayer(attacker, target):
     if target["defense"]:
         damage = max(0, attacker["damage"] - target["defensePower"])
         setPlayer(target, "defense", False)
-        score = 0.8
+        score = round(attacker['score']+1-1/target['defensePower'],2)
     else:
         damage = attacker["damage"]
         score = 1
     
     # hit target
-
     setPlayer(target, "health", max(0, target["health"] - damage))
     # tambah skor attacker
     setPlayer(attacker, "score", attacker["score"] + score)
-    
-    
+        
 
 def displayMatchResult():
     global PlayerList
